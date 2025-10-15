463b26bb6565e6b2d7a9cfdde45ad618c1ca436d
Lawrence Mindanao <mindanaolawrence86@gmail.com>
2025-10-15 15:39:12 +0800

    added color the h2 in missionvision

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -130,14 +130,14 @@ function Home() {
         </div>
       </div>
       <div className="container-missionvission m-4">
-        <h2>Core Values</h2>
+        <h2 className="text-success">Core Values</h2>
         <p>As God-fearing  institution respecting multi faith of people PnC adheres to the following core values 
             P - Personal dignity 
             N - nurturing community 
             C - commitment to excellence.</p>
-        <h2>Mission</h2>
+        <h2 className="text-success">Mission</h2>
         <p>As an-institution of higher learning, UC (PnC)  is committed to equip individual with knowledge, skills and values that will enable them to achieve their professionals goals and provide leadership and service for national development.</p>
-        <h2>Vision</h2>
+        <h2 className="text-success">Vision</h2>
         <p>A premier institution higher learning in Region IV - developing globally competitive and valued-laden professionals and leaders instrumental to community development and nation building.</p>
       </div>
 

e95b5c79c829a13aa7538285c8aa55672f27ce07
Lawrence Mindanao <mindanaolawrence86@gmail.com>
2025-10-15 14:44:59 +0800

    adding the mission,vision and core values since wala siya

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,7 @@ function Home() {
   const prevSlide = () => setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
 
   return (
-    <>
+    <div className="Home">
       {/* Hero Section */}
       <div
         className="hero-section position-relative text-white"
@@ -129,8 +129,20 @@ function Home() {
           </div>
         </div>
       </div>
+      <div className="container-missionvission m-4">
+        <h2>Core Values</h2>
+        <p>As God-fearing  institution respecting multi faith of people PnC adheres to the following core values 
+            P - Personal dignity 
+            N - nurturing community 
+            C - commitment to excellence.</p>
+        <h2>Mission</h2>
+        <p>As an-institution of higher learning, UC (PnC)  is committed to equip individual with knowledge, skills and values that will enable them to achieve their professionals goals and provide leadership and service for national development.</p>
+        <h2>Vision</h2>
+        <p>A premier institution higher learning in Region IV - developing globally competitive and valued-laden professionals and leaders instrumental to community development and nation building.</p>
+      </div>
+
       
-    </>
+    </div>
   );
 }
 
